00795274984d43e8f2117d81b089b67b071ddc35
pauCM-es <costamingarro@gmail.com>
2024-06-29 19:51:11 +0200

    FIX: connect error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KeycloakModule } from './keycloak/keycloak.module';
+import { KeycloakController } from './keycloak/keycloak.controller';
+import { KcUserService } from './keycloak/kc-user.service';
 
 @Module({
-  imports: [KeycloakModule],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
